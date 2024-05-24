6beb0a5e2ace2f90881a40f2843bec34355fdc51
Trevor Rudolph <cool.test.cool@gmail.com>
2024-05-24 12:13:57 -0400

    fix lowmem gameid for saves

--- patches/source/dolphin_os.h
+++ patches/source/dolphin_os.h
@@ -1,10 +1,62 @@
 #include <gctypes.h>
 #include "config.h"
 
+#include "../../cubeboot/include/gcm.h"
+
 // gcbool
 typedef unsigned int BOOL;
 
 typedef void* (*OSThreadStartFunction)(void*);
+
+
+struct dolphin_debugger_info {
+	uint32_t		present;
+	uint32_t		exception_mask;
+	uint32_t		exception_hook_address;
+	uint32_t		saved_lr;
+	unsigned char		__pad1[0x10];
+} __attribute__ ((__packed__));
+
+struct dolphin_lowmem {
+	struct gcm_disk_info	b_disk_info;
+
+	uint32_t		a_boot_magic;
+	uint32_t		a_version;
+
+	uint32_t		b_physical_memory_size;
+	uint32_t		b_console_type;
+
+	uint32_t		a_arena_lo;
+	uint32_t		a_arena_hi;
+	void			*a_fst;
+	uint32_t		a_fst_max_size;
+
+	struct dolphin_debugger_info a_debugger_info;
+	unsigned char		hook_code[0x60];
+
+	uint32_t		o_current_os_context_phys;
+	uint32_t		o_previous_os_interrupt_mask;
+	uint32_t		o_current_os_interrupt_mask;
+
+	uint32_t		tv_mode;
+	uint32_t		b_aram_size;
+
+	void			*o_current_os_context;
+	void			*o_default_os_thread;
+	void			*o_thread_queue_head;
+	void			*o_thread_queue_tail;
+	void			*o_current_os_thread;
+
+	uint32_t		a_debug_monitor_size;
+	void			*a_debug_monitor;
+
+	uint32_t		a_simulated_memory_size;
+
+	void			*a_bi2;
+
+	uint32_t		b_bus_clock_speed;
+	uint32_t		b_cpu_clock_speed;
+} __attribute__ ((__packed__));
 
 void OSYieldThread();
 BOOL dolphin_OSCreateThread(void *thread, OSThreadStartFunction func, void* param, void* stack, u32 stackSize, s32 priority, u16 attr);
